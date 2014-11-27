59271610e227e11f548765564125ee47cfdb877b
ymofen <185511468@qq.com>
2014-11-27 13:38:28 +0800

    * ASyncTcpClient

--- samples/SimpleConsole/IMPlugin/DAysncTcpClient.cs
+++ samples/SimpleConsole/IMPlugin/DAysncTcpClient.cs
@@ -8,9 +8,40 @@ namespace IMPlugin
 {
     public class DAysncTcpClient
     {
-        private Socket rawSocket;
+        private Socket rawSocket = null;
 
-        String Host;
-        Int16 Port;
+        private String host;
+
+        private void CheckCreateRawSocket()
+        {
+            if (rawSocket == null)
+            {
+                rawSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IPv4);
+                rawSocket.IOControl(IOControlCode.NonBlockingIO, 1, 0);
+            }
+        }
+
+        public String Host
+        {
+            get { return host; }
+            set { host = value; }
+        }
+
+        private Int16 port;
+
+        public Int16 Port
+        {
+            get { return port; }
+            set { port = value; }
+        }
+
+        public void Connect(int timeOut)
+        {
+            CheckCreateRawSocket();
+            
+            rawSocket.Connect(host, port);
+        }
+
+
     }
 }
