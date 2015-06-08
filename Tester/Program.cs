6b9b6716fd5176d9a396d0135d757b675c4cee0b
ymofen <185511468@qq.com>
2015-06-08 17:31:41 +0800

    + 添加Tester工程

---
+++ Tester/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyBean.Core.Helper;
+
+namespace Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "01.02A";
+            
+            Console.WriteLine(s.LastStr("."));
+            Console.WriteLine(s.ExtractLastNumStr(1));
+      
+            Console.WriteLine(s.IncValue(1, 0));
+
+            string s2 = null;
+            if (s2.IsNullOrEmpty())
+            {
+                Console.WriteLine("s2 is null or empty!");
+            }
+
+            Console.Read();
+        }
+    }
+}
