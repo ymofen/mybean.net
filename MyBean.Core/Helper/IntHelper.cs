cc6d9c7d0cc0ca45523557c0a734da3748a5a5cd
ymofen <185511468@qq.com>
2015-06-16 14:58:30 +0800

    * MyBean

--- MyBean.Core/Helper/IntHelper.cs
+++ MyBean.Core/Helper/IntHelper.cs
@@ -7,6 +7,12 @@ namespace MyBean.Core.Helper
 {
     public static class IntHelper
     {
+        /// <summary>
+        ///  字符串转整数,如果转换异常返回默认值
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
         public static int ToInteger(this string s, int defaultValue)
         {
             if ((s==null) || (s.Length == 0))
@@ -26,6 +32,20 @@ namespace MyBean.Core.Helper
                 }
             }        
             
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="v"></param>
+        /// <param name="width"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static string ToWidth(this int v, int width, char c)
+        {
+            string rvalue = v.ToString();
+            int j = width - rvalue.Length;
+            return StringHelper.RepeatStr(c.ToString(), j) + rvalue;
         }
 
     }
--- Tester/Program.cs
+++ Tester/Program.cs
@@ -4,25 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MyBean.Core.Helper;
+using MyBean.Core.Helper;
 
 namespace Tester
 {
+    class Person
+    {
+        public string Name{set;get;}
+    }
+
+    class TestProperty
+    {
+        private Person aman = null;
+        public Person AMan { get { return aman; } }
+
+        public void MakeAMan()
+        {
+            aman = new Person();
+            aman.Name = "张三"; 
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            string s = "01.02A";
-            
-            Console.WriteLine(s.LastStr("."));
-            Console.WriteLine(s.ExtractLastNumStr(1));
-      
-            Console.WriteLine(s.IncValue(1, 0));
+            //TestProperty tester = new TestProperty();
+            //tester.MakeAMan();
+            //Console.WriteLine(tester.AMan);
 
-            string s2 = null;
-            if (s2.IsNullOrEmpty())
-            {
-                Console.WriteLine("s2 is null or empty!");
-            }
+
+            int i = 1;
+            char c = ' ';
+            Console.WriteLine(i.ToWidth(4, c));
+
+
+            //string s = "01.02A";
+            
+            //Console.WriteLine(s.LastStr("."));
+            //Console.WriteLine(s.ExtractLastNumStr(1));
+      
+            //Console.WriteLine(s.IncValue(1, 0));
+
+            //string s2 = null;
+            //if (s2.IsNullOrEmpty())
+            //{
+            //    Console.WriteLine("s2 is null or empty!");
+            //}
 
             Console.Read();
         }
